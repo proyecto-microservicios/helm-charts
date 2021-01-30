87a0fe79f3a0d15f165dd1ff800601268fbbbc0d
Bruno Gonzalez <bruno.prp@gmail.com>
2021-01-30 20:32:15 -0300

    fix script, v0.1.2

--- update-chart.sh
+++ update-chart.sh
@@ -1,4 +1,4 @@
 CHART=$1
 VERSION=$2
 
-helm package $CHART --version $2 && mv $CHART-$VERSION.tgz docs && helm repo index docs --url https://proyecto-microservicios.github.com/helm-charts
+helm package $CHART --version $2 --app-version $2 && cp $CHART-$VERSION.tgz docs && helm repo index docs --url https://proyecto-microservicios.github.com/helm-charts
